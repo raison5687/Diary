11075b652d540c7862ab893f2ed151ab97922b5c
우상현 <max99808@naver.com>
2021-11-09 21:46:35 +0900

    오류점검(다수)

--- app/src/main/java/com/example/deardiary/SettingFragment.java
+++ app/src/main/java/com/example/deardiary/SettingFragment.java
@@ -2,6 +2,8 @@ package com.example.deardiary;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,9 @@ import com.example.deardiary.databinding.FragmentSettingBinding;
 public class SettingFragment extends Fragment {
 
     public FragmentSettingBinding binding;
+    final static int TAKE_PICTURE = 1;
+    String mCurrentPhotoPath;
+    final static int REQUEST_TAKE_PHOTO = 1;
 
     public SettingFragment(){}
 
@@ -33,6 +38,38 @@ public class SettingFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentSettingBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if(checkSelfPermission(Manifest.permission.CAMERA) ==
+//                    PackageManager.PERMISSION_GRANTED &&
+//                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+//                            PackageManager.PERMISSION_GRANTED) {
+//                Log.d(TAG, "권한 설정 완료");
+//            } else {
+//                Log.d(TAG, "권한 설정 요청");
+//                FragmentManager.requestPermissions(SettingFragment.this, new String[]
+//                        {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+//            }
+//        }
+//
+//        binding.btnProfile.setOnTouchListener({
+//        @Override
+//        public void onTouch (View v){
+//            switch (v.getId()) {
+//                case btn_Profile:
+//                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(cameraIntent, TAKE_PICTURE);
+//                    break;
+//            }
+//        }
+//        });
+//
+//        );
+
+
         return view;
     }
+
+
+
 }
