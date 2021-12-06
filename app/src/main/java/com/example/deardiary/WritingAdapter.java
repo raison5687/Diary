ec64f7f26e992e0b66fda0ebdc342fdf1a7bbe93
최예원 <choiyewon127@gmail.com>
2021-12-06 14:42:59 +0900

    Merge branch 'master' of https://github.com/raison5687/DearDiary
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/deardiary/CalenderFragment.java
    #	app/src/main/java/com/example/deardiary/HomeFragment.java
    #	app/src/main/res/layout/fragment_home.xml

---
+++ app/src/main/java/com/example/deardiary/WritingAdapter.java
@@ -0,0 +1,4 @@
+package com.example.deardiary;
+
+public class WritingAdapter {
+}
--- app/src/main/java/com/example/deardiary/WritingFragment.java
+++ app/src/main/java/com/example/deardiary/WritingFragment.java
@@ -1,34 +1,20 @@
 package com.example.deardiary;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.deardiary.databinding.FragmentWritingBinding;
 
-import static android.app.Activity.RESULT_OK;
-
 public class WritingFragment extends Fragment {
-
+    private RecyclerView writingRecyclerView;
     public FragmentWritingBinding binding;
-    Uri picture;
+    WritingAdapter writingAdapter;
 
     public WritingFragment(){}
 
@@ -50,56 +36,10 @@ public class WritingFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentWritingBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        binding.btnAddPic.setOnClickListener(v -> addPic());
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
-                PackageManager.PERMISSION_GRANTED) {
-            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
-        }
+
+        writingAdapter = new WritingAdapter();
+
         return view;
-    }
-
-    ActivityResultLauncher<Intent> resultLauncher =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        Intent data = result.getData();
-                        Log.i("TEST", "data: " + data);
-
-                        if (result.getResultCode() == RESULT_OK && null != data) {
-                            Uri selectedImage = data.getData();
-                            picture = selectedImage;
-                            Bitmap bitmap = loadBitmap(selectedImage);
-                            binding.imgInsertPic.setImageBitmap(bitmap);
-                        }
-                    });
-
-    private ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    Toast.makeText(getContext(), "권한이 설정되었습니다", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getContext(), "권한이 설정되지 않았습니다. 권한이 없으므로 앱을 종료합니다", Toast.LENGTH_SHORT).show();
-//                    finish();
-                }
-            });
-
-    public void addPic() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        resultLauncher.launch(intent);
-    }
-
-    private Bitmap loadBitmap(Uri uri) {
-        String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-        Cursor cursor = getActivity().getContentResolver().query(uri,
-                filePathColumn, null, null, null);
-        cursor.moveToFirst();
-
-        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-        String picturePath = cursor.getString(columnIndex);
-        cursor.close();
-
-        return BitmapFactory.decodeFile(picturePath);
     }
 
 }
