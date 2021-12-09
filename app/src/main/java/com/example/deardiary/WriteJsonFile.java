b417fbb5a5987e74653d02cb91d82eb9ac936cd6
이승현 <raison55687@gmail.com>
2021-12-09 19:40:03 +0900

    일기장 데이터 입력 구현(에러 : 사진이 3장이하), RecylcerView 홈화면으로 데이터 출력 미구현

---
+++ app/src/main/java/com/example/deardiary/WriteJsonFile.java
@@ -0,0 +1,20 @@
+package com.example.deardiary;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WriteJsonFile {
+
+    public static void main(String[] args){
+
+        JSONObject obj = new JSONObject();
+//        obj.put("title", DiaryModel.class.)
+
+
+    }
+}
--- app/src/main/java/com/example/deardiary/WritingFragment.java
+++ app/src/main/java/com/example/deardiary/WritingFragment.java
@@ -2,10 +2,12 @@ package com.example.deardiary;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -23,11 +25,34 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.deardiary.databinding.FragmentWritingBinding;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WritingFragment extends Fragment {
     public FragmentWritingBinding binding;
+    Uri image1 = null;
+    Uri image2 = null;
+    Uri image3 = null;
+    private ArrayList<DiaryModel> data = new ArrayList<>();
 
     public WritingFragment(){}
 
@@ -50,7 +75,9 @@ public class WritingFragment extends Fragment {
         binding = FragmentWritingBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         binding.btnWritingPlus.setOnClickListener(v -> add());
-
+        binding.btnWritingSave.setOnClickListener(v -> save());
+        String jsonString =  readFile(getContext(), "Diaryfile.json");
+        data = new Gson().fromJson(jsonString, new TypeToken<ArrayList<DiaryModel>>(){}.getType());
         return view;
     }
     private void add(){
@@ -64,11 +91,19 @@ public class WritingFragment extends Fragment {
                 Intent data = result.getData();
                 if (result.getResultCode() == RESULT_OK && null != data) {
                     Uri selectedImage = data.getData();
+                    if(image1 == null) {
+                        image1 = selectedImage;
+                    } else if(image2 == null) {
+                        image2 = selectedImage;
+                    } else {
+                        image3 = selectedImage;
+                    }
                     Bitmap bitmap = loadBitmap(selectedImage);
                     if(binding.img1.getVisibility() != View.VISIBLE) {
                         binding.img1.setImageBitmap(bitmap);
                         binding.img1.setVisibility(View.VISIBLE);
-                    } else if(binding.img2.getVisibility() != View.VISIBLE) {
+                    }
+                    else if(binding.img2.getVisibility() != View.VISIBLE) {
                         binding.img2.setImageBitmap(bitmap);
                         binding.img2.setVisibility(View.VISIBLE);
                     } else {
@@ -91,5 +126,72 @@ public class WritingFragment extends Fragment {
         return BitmapFactory.decodeFile(picturePath);
     }
 
+    public void save()
+    {
+        String title = binding.editTxtWritingTitle.getText().toString();
+        String content = binding.editTxtWritingContent.getText().toString();
+        String img1 = image1.toString();
+        String img2 = image2.toString();
+        String img3 = image3.toString();
+        Long date = System.currentTimeMillis();
+        DiaryModel model = new DiaryModel(title, content, img1, img2, img3, date);
+        this.data.add(model);
+        try {
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("title", title);
+//            map.put("content", content);
+//            map.put("img1", img1);
+//            map.put("img2", img2);
+//            map.put("img3", img3);
+//            map.put("date", date);
 
+            String jsonString = new Gson().toJson(data);
+            writeFile(getContext(), "Diaryfile.json", jsonString);
+
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "파일을 못찾음", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static void writeFile(Context context, String filename, String data) {
+        try {
+            FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(data.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readFile(Context context, String filename) {
+        FileInputStream fis = null;
+        try {
+            fis = context.openFileInput(filename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+        return readStream(fis);
+    }
+
+    public static String readStream(InputStream inputStream) {
+        String contents = "";
+        InputStreamReader inputStreamReader =
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+
+        } finally {
+            contents = stringBuilder.toString().trim();
+        }
+        return contents;
+    }
 }
+
